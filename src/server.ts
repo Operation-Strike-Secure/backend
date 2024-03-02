31650f46423a492d9a5bcf0ad57b7ac55e3047ed
mistercouscous <erwan-baillon@orange.fr>
2024-03-02 16:08:07 +0100

    ADD linter eslint

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello World')
+})
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+  console.log('Server is running on port 3000')
+})
